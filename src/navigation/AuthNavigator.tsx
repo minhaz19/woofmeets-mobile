3c3de09da4ca44285068446dc8dff092e5e715c3
meer habib <meerhabib200@gmail.com>
2022-07-01 03:51:42 +0000

    Auth screens

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import SetNewPassword from '../screens/auth/SetNewPassword';
-// import Login from '../screens/auth/Login';
+import Login from '../screens/auth/Login';
 
 const Stack = createStackNavigator();
 
@@ -10,7 +9,7 @@ function AuthNavigator() {
     <Stack.Navigator initialRouteName="AuthHomeScreen">
       <Stack.Screen
         name="AuthHomeScreen"
-        component={SetNewPassword}
+        component={Login}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
