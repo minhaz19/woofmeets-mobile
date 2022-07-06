1a328478cbaf28702e221c957ac0e5d658ec021e
Minhazul abedin <minhazulabedin44@gmail.com>
2022-07-06 12:16:54 +0000

    Reusable component and boarding page done

---
+++ src/components/ScreenComponent/SelectService/DropInVisits/DropInVisits.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const DropInVisits = () => {
+  return (
+    <View>
+      <Text>DropInVisits</Text>
+    </View>
+  );
+};
+
+export default DropInVisits;
+
+const styles = StyleSheet.create({});
