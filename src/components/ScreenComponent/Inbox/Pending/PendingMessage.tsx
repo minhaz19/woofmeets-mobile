4998d2909a56a12cbc368d21a59b024c25097ba0
Minhazul Abedin <minhaz@algosolver.com>
2022-07-27 11:43:30 +0600

    pending message

--- src/components/ScreenComponent/Inbox/Pending/PendingMessage.tsx
+++ src/components/ScreenComponent/Inbox/Pending/PendingMessage.tsx
@@ -1,14 +1,38 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
-import {SCREEN_WIDTH} from '../../../../constants/WindowSize';
+import {SCREEN_HEIGHT, SCREEN_WIDTH} from '../../../../constants/WindowSize';
+import HeaderText from '../../../common/text/HeaderText';
+import DescriptionText from '../../../common/text/DescriptionText';
 
 const PendingMessage = () => {
   return (
     <View style={styles.container}>
-        <View>
-            
+      <View style={styles.flexContainer}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../../../../assets/image/pet/mypet.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
         </View>
-      <Text>PendingMessage</Text>
+        <View>
+          <HeaderText text="Steve Goodman" textStyle={styles.textHeader} />
+          <DescriptionText
+            text="Steve Goodman"
+            textStyle={styles.textDescription}
+          />
+          <DescriptionText
+            text="Steve Goodman"
+            textStyle={styles.textDescription}
+          />
+        </View>
+        <View>
+          <DescriptionText
+            text="Steve Goodman"
+            textStyle={styles.textDescription}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -19,5 +43,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: SCREEN_WIDTH <= 380 ? '6%' : SCREEN_WIDTH <= 600 ? '5%' : '2%',
+  },
+  image: {
+    borderRadius: 100,
+    width: SCREEN_WIDTH <= 380 ? 30 : SCREEN_WIDTH <= 600 ? 30 : 40,
+    height: SCREEN_WIDTH <= 380 ? 30 : SCREEN_WIDTH <= 600 ? 30 : 40,
+  },
+  flexContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // alignItems: 'center',
+  },
+  imageContainer: {
+    width: SCREEN_WIDTH <= 380 ? 25 : SCREEN_WIDTH <= 600 ? 30 : 40,
   },
 });
