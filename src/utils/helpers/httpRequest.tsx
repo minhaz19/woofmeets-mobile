c777e616a6c807bd841c86630f72e9ea049d9b24
Tanvir Islam <mdtanvirislams@gmail.com>
2022-12-02 23:53:52 +1100

    fix: resolve conflict

--- src/utils/helpers/httpRequest.tsx
+++ src/utils/helpers/httpRequest.tsx
@@ -1,6 +1,6 @@
-const baseUrl = 'https://woof-api.hirebeet.com/v1';
-// const baseUrl = 'https://api.woofmeets.com/v1';
+// const baseUrl = 'https://woof-api.hirebeet.com/v1';
+const baseUrl = 'https://api.woofmeets.com/v1';
 export const msgUrl = 'https://msg.woofmeets.com';
-export const baseUrlV = 'https://woof-api.hirebeet.com';
-// export const baseUrlV = 'https://api.woofmeets.com';
+// export const baseUrlV = 'https://woof-api.hirebeet.com';
+export const baseUrlV = 'https://api.woofmeets.com';
 export default baseUrl;
